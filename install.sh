39e4f6ce32867e2e6967d2ac111486d8574fbdb2
Jesse Squires <jesse.d.squires@gmail.com>
2014-11-13 21:59:17 -0800

    edit install

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo 'Copying script to /Applications/Adobe Photoshop CC 2014/Preset/Scripts/....'
+
 cp Create-iOS-icons.jsx /Applications/Adobe\ Photoshop\ CC\ 2014/Presets/Scripts/
+
+echo '🎉   Done! 🍺'
