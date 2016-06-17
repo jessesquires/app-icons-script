ee0b8c5673a167f9158fa6f7dec7f649516ca9a7
Jesse Squires <jesse.d.squires@gmail.com>
2016-06-16 22:23:31 -0700

    imessage stickers

--- Create-iMessage-stickers.jsx
+++ Create-iMessage-stickers.jsx
@@ -19,7 +19,7 @@ var initialPrefs = app.preferences.rulerUnits;
 
 function main() {
 	//	prompt user to select source file, cancel returns null
-	var sourceFile = File.openDialog("Select a PNG file that is 1024x768.", "*.png", false);
+	var sourceFile = File.openDialog("Select a 1:1 sqaure PNG file that is at least 618x618.", "*.png", false);
 	if (sourceFile == null)  { 
 		// user canceled
 		return;
@@ -33,8 +33,8 @@ function main() {
 
 	app.preferences.rulerUnits = Units.PIXELS;
 
-	if (doc.width < 1024 || doc.height < 768) {
-		alert("What the fuck is this?!\nImage failed validation. Please select a PNG file that is at least 1024x768.");
+	if (doc.width != doc.height || doc.width < 618 || doc.height < 618) {
+		alert("What the fuck is this?!\nImage failed validation. Please select a 1:1 sqaure PNG file that is at least 618x618.");
 		restorePrefs();
 		return;
 	}
@@ -57,29 +57,9 @@ function main() {
 	doc.info = null;
 	
 	var icons = [
-	   {"name": "icon", "w":1024, "h":768},
-        
-	   {"name": "messages", "w":27, "h":20},
-	   {"name": "messages@2x", "w":54, "h":40},
-	   {"name": "messages@3x", "w":81, "h":60},
-        
-	   {"name": "messages-2", "w":32, "h":24},
-	   {"name": "messages-2@2x", "w":64, "h":48},
-	   {"name": "messages-2@3x", "w":96, "h":72},
-        
-	   {"name": "iphone-settings@2x", "w":58, "h":58},
-	   {"name": "iphone-settings@3x", "w":87, "h":87},
-        
-	   {"name": "iphone@2x", "w":120, "h":90},
-	   {"name": "iphone@3x", "w":180, "h":135},
-        
-       {"name": "ipad-settings", "w":29, "h":29},
-	   {"name": "ipad-settings@2x", "w":58, "h":58},
-        
-	   {"name": "ipad", "w":67, "h":50},
-	   {"name": "ipad@2x", "w":134, "h":100},
-        
-	   {"name": "ipad-pro@2x", "w":148, "h":110},
+		{"name": "sticker-100@3x", "size":300},
+		{"name": "sticker-136@3x", "size":408},
+		{"name": "sticker-206@3x", "size":618},
 	];
 
 	var initialState = doc.activeHistoryState; 
@@ -87,7 +67,7 @@ function main() {
 	for (var i = 0; i < icons.length; i++) {
 		var eachIcon = icons[i];
 
-		doc.resizeImage(eachIcon.w, eachIcon.h, null, ResampleMethod.BICUBICSHARPER);
+		doc.resizeImage(eachIcon.size, eachIcon.size, null, ResampleMethod.BICUBICSHARPER);
 
 		var destFileName = eachIcon.name + ".png";
 
--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ echo 'Copying script to /Applications/Adobe Photoshop CC 2014/Preset/Scripts/...
 
 cp Create-iOS-icons.jsx /Applications/Adobe\ Photoshop\ CC\ 2014/Presets/Scripts/
 cp Create-iMessage-icons.jsx /Applications/Adobe\ Photoshop\ CC\ 2014/Presets/Scripts/
+cp Create-iMessage-stickers.jsx /Applications/Adobe\ Photoshop\ CC\ 2014/Presets/Scripts/
 
 echo '🎉   Done! 🍺'
