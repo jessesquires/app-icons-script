a3f03378da862f30e1a7f1894e5042cd525d8863
Asher Peruscini <asherperuscini@gmail.com>
2017-05-26 08:18:17 -0700

    updated the file paths in ./install.sh for Creative Cloud 2017

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo 'Copying script to /Applications/Adobe Photoshop CC 2014/Preset/Scripts/....'
+echo 'Copying script to /Applications/Adobe Photoshop CC 2017/Preset/Scripts/....'
 
-cp Create-iOS-icons.jsx /Applications/Adobe\ Photoshop\ CC\ 2014/Presets/Scripts/
-cp Create-iMessage-icons.jsx /Applications/Adobe\ Photoshop\ CC\ 2014/Presets/Scripts/
-cp Create-iMessage-stickers.jsx /Applications/Adobe\ Photoshop\ CC\ 2014/Presets/Scripts/
+cp Create-iOS-icons.jsx /Applications/Adobe\ Photoshop\ CC\ 2017/Presets/Scripts/
+cp Create-iMessage-icons.jsx /Applications/Adobe\ Photoshop\ CC\ 2017/Presets/Scripts/
+cp Create-iMessage-stickers.jsx /Applications/Adobe\ Photoshop\ CC\ 2017/Presets/Scripts/
 
 echo '🎉   Done! 🍺'
