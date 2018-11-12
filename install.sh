9fe8ff4ceb57c033fca4fcd5b7bd2de8efa0ab4c
Jesse Squires <jesse@jessesquires.com>
2018-11-12 10:23:26 -0800

    update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-echo 'Copying script to /Applications/Adobe Photoshop CC 2014/Preset/Scripts/....'
+echo 'Copying scripts to /Applications/Adobe Photoshop CC 2014/Preset/Scripts/....'
 
-cp Create-iOS-icons.jsx /Applications/Adobe\ Photoshop\ CC\ 2014/Presets/Scripts/
-cp Create-iMessage-icons.jsx /Applications/Adobe\ Photoshop\ CC\ 2014/Presets/Scripts/
-cp Create-iMessage-stickers.jsx /Applications/Adobe\ Photoshop\ CC\ 2014/Presets/Scripts/
+cp *.jsx /Applications/Adobe\ Photoshop\ CC\ 2014/Presets/Scripts/
 
 echo '🎉   Done! 🍺'
